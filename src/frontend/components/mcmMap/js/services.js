6ca203dde4948d608459b987fe7c0d6490ae740e
Sinisha Rudan <sinisa.rudan@gmail.com>
2015-04-29 05:50:19 +0200

    added additional copies of selector logic ('mcm_map_select_variable_quantity', etc) based on 'mcm_map_select_assumption'

--- src/frontend/components/mcmMap/js/services.js
+++ src/frontend/components/mcmMap/js/services.js
@@ -354,5 +354,340 @@ mcmMapServices.provider('McmMapAssumptionService', {
 	}]
 });
 
+mcmMapServices.provider('McmMapVariableQuantityService', {
+	// privateData: "privatno",
+	$get: [/*'$q', '$rootScope', */
+	function(/*$q, $rootScope*/) {
+		var variableQuantitysDescs = [
+			{
+				name: "variableQuantity_1"
+			},
+			{
+				name: "variableQuantity_2"
+			},
+			{
+				name: "variableQuantity_3"
+			},
+			{
+				name: "variableQuantity_12"
+			},
+			{
+				name: "variableQuantity_13"
+			},
+			{
+				name: "variableQuantity_123"
+			},
+			{
+				name: "variableQuantity_14"
+			},
+			{
+				name: "variableQuantity_145"
+			},
+			{
+				name: "variableQuantity_12345"
+			},
+			{
+				name: "variableQuantity_1235"
+			},
+			{
+				name: "variableQuantity_143"
+			},
+			{
+				name: "variableQuantity_1451"
+			},
+			{
+				name: "variableQuantity_1245"
+			}
+		];
+
+		// var that = this;
+		return {
+			getVariableQuantitysDescs: function(){
+				return variableQuantitysDescs;
+			},
+
+			getVariableQuantitysDesByName: function(nameSubStr){
+				var returnedVariableQuantitys = [];
+				for(var i in variableQuantitysDescs){
+					var variableQuantity = variableQuantitysDescs[i];
+					if(variableQuantity.name.indexOf(nameSubStr) > -1){
+						returnedVariableQuantitys.push(variableQuantity);
+					}
+				}
+				return returnedVariableQuantitys;
+			},
+
+		};
+	}]
+});
+
+mcmMapServices.provider('McmMapVariableOperatorService', {
+	// privateData: "privatno",
+	$get: [/*'$q', '$rootScope', */
+	function(/*$q, $rootScope*/) {
+		var variableOperatorsDescs = [
+			{
+				name: "variableOperator_1"
+			},
+			{
+				name: "variableOperator_2"
+			},
+			{
+				name: "variableOperator_3"
+			},
+			{
+				name: "variableOperator_12"
+			},
+			{
+				name: "variableOperator_13"
+			},
+			{
+				name: "variableOperator_123"
+			},
+			{
+				name: "variableOperator_14"
+			},
+			{
+				name: "variableOperator_145"
+			},
+			{
+				name: "variableOperator_12345"
+			},
+			{
+				name: "variableOperator_1235"
+			},
+			{
+				name: "variableOperator_143"
+			},
+			{
+				name: "variableOperator_1451"
+			},
+			{
+				name: "variableOperator_1245"
+			}
+		];
+
+		// var that = this;
+		return {
+			getVariableOperatorsDescs: function(){
+				return variableOperatorsDescs;
+			},
+
+			getVariableOperatorsDesByName: function(nameSubStr){
+				var returnedVariableOperators = [];
+				for(var i in variableOperatorsDescs){
+					var variableOperator = variableOperatorsDescs[i];
+					if(variableOperator.name.indexOf(nameSubStr) > -1){
+						returnedVariableOperators.push(variableOperator);
+					}
+				}
+				return returnedVariableOperators;
+			},
+
+		};
+	}]
+});
+
+mcmMapServices.provider('McmMapObjectService', {
+	// privateData: "privatno",
+	$get: [/*'$q', '$rootScope', */
+	function(/*$q, $rootScope*/) {
+		var objectsDescs = [
+			{
+				name: "object_1"
+			},
+			{
+				name: "object_2"
+			},
+			{
+				name: "object_3"
+			},
+			{
+				name: "object_12"
+			},
+			{
+				name: "object_13"
+			},
+			{
+				name: "object_123"
+			},
+			{
+				name: "object_14"
+			},
+			{
+				name: "object_145"
+			},
+			{
+				name: "object_12345"
+			},
+			{
+				name: "object_1235"
+			},
+			{
+				name: "object_143"
+			},
+			{
+				name: "object_1451"
+			},
+			{
+				name: "object_1245"
+			}
+		];
+
+		// var that = this;
+		return {
+			getObjectsDescs: function(){
+				return objectsDescs;
+			},
+
+			getObjectsDesByName: function(nameSubStr){
+				var returnedObjects = [];
+				for(var i in objectsDescs){
+					var object = objectsDescs[i];
+					if(object.name.indexOf(nameSubStr) > -1){
+						returnedObjects.push(object);
+					}
+				}
+				return returnedObjects;
+			},
+
+		};
+	}]
+});
+
+mcmMapServices.provider('McmMapProcessService', {
+	// privateData: "privatno",
+	$get: [/*'$q', '$rootScope', */
+	function(/*$q, $rootScope*/) {
+		var processsDescs = [
+			{
+				name: "process_1"
+			},
+			{
+				name: "process_2"
+			},
+			{
+				name: "process_3"
+			},
+			{
+				name: "process_12"
+			},
+			{
+				name: "process_13"
+			},
+			{
+				name: "process_123"
+			},
+			{
+				name: "process_14"
+			},
+			{
+				name: "process_145"
+			},
+			{
+				name: "process_12345"
+			},
+			{
+				name: "process_1235"
+			},
+			{
+				name: "process_143"
+			},
+			{
+				name: "process_1451"
+			},
+			{
+				name: "process_1245"
+			}
+		];
+
+		// var that = this;
+		return {
+			getProcesssDescs: function(){
+				return processsDescs;
+			},
+
+			getProcesssDesByName: function(nameSubStr){
+				var returnedProcesss = [];
+				for(var i in processsDescs){
+					var process = processsDescs[i];
+					if(process.name.indexOf(nameSubStr) > -1){
+						returnedProcesss.push(process);
+					}
+				}
+				return returnedProcesss;
+			},
+
+		};
+	}]
+});
+
+mcmMapServices.provider('McmMapGridService', {
+	// privateData: "privatno",
+	$get: [/*'$q', '$rootScope', */
+	function(/*$q, $rootScope*/) {
+		var gridsDescs = [
+			{
+				name: "grid_1"
+			},
+			{
+				name: "grid_2"
+			},
+			{
+				name: "grid_3"
+			},
+			{
+				name: "grid_12"
+			},
+			{
+				name: "grid_13"
+			},
+			{
+				name: "grid_123"
+			},
+			{
+				name: "grid_14"
+			},
+			{
+				name: "grid_145"
+			},
+			{
+				name: "grid_12345"
+			},
+			{
+				name: "grid_1235"
+			},
+			{
+				name: "grid_143"
+			},
+			{
+				name: "grid_1451"
+			},
+			{
+				name: "grid_1245"
+			}
+		];
+
+		// var that = this;
+		return {
+			getGridsDescs: function(){
+				return gridsDescs;
+			},
+
+			getGridsDesByName: function(nameSubStr){
+				var returnedGrids = [];
+				for(var i in gridsDescs){
+					var grid = gridsDescs[i];
+					if(grid.name.indexOf(nameSubStr) > -1){
+						returnedGrids.push(grid);
+					}
+				}
+				return returnedGrids;
+			},
+
+		};
+	}]
+});
+
 
 }()); // end of 'use strict';
