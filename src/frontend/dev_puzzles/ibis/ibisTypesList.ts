9ac263b63427e76ce7ad6bd338b544c03b80e5bf
Sasha Rudan <mprinc@gmail.com>
2016-10-12 14:15:11 +0200

    Added ibis puzzle

---
+++ src/frontend/dev_puzzles/ibis/ibisTypesList.ts
@@ -0,0 +1,76 @@
+import {Component, Inject} from '@angular/core';
+import {NgIf, CORE_DIRECTIVES} from "@angular/common";
+import {MATERIAL_DIRECTIVES} from 'ng2-material';
+import {FORM_DIRECTIVES} from '@angular/forms';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+
+import {KnalledgeMapViewService} from '../../app/components/knalledgeMap/knalledgeMapViewService';
+import {KnalledgeMapPolicyService} from '../../app/components/knalledgeMap/knalledgeMapPolicyService';
+import {GlobalEmitterServicesArray} from '../../app/components/collaboPlugins/GlobalEmitterServicesArray';
+
+@Component({
+    selector: 'ibis-types-list',
+    providers: [
+        //MATERIAL_PROVIDERS
+    ],
+    directives: [
+      MATERIAL_DIRECTIVES,
+      // MdList, MdListItem, MdContent, MdButton, MdSwitch,
+      NgIf, FORM_DIRECTIVES,
+      // MdRadioButton, MdRadioGroup,
+      //
+      MD_INPUT_DIRECTIVES
+   ],
+    moduleId: module.id,
+    templateUrl: 'partials/ibisTypes-list.tpl.html',
+    styles: [`
+    `]
+})
+export class IbisTypesList {
+  public items:Array<any> = [];
+  public selectedItem:any = null;
+  private componentShown:boolean = true;
+  private ibisTypesService;
+  private viewConfig:any;
+  private policyConfig:any;
+  private knalledgeNodeTypeChanged: string = "knalledgeNodeTypeChanged";
+
+
+  constructor(
+    @Inject('IbisTypesService') _IbisTypesService_,
+    @Inject('KnalledgeMapViewService') knalledgeMapViewService:KnalledgeMapViewService,
+    @Inject('KnalledgeMapPolicyService') knalledgeMapPolicyService:KnalledgeMapPolicyService,
+    @Inject('GlobalEmitterServicesArray') private globalEmitterServicesArray:GlobalEmitterServicesArray
+  ) {
+      // console.log('[IbisTypesList]');
+      this.ibisTypesService = _IbisTypesService_;
+
+      this.items = this.ibisTypesService.getTypes();
+      this.selectedItem = this.ibisTypesService.getActiveType();
+      this.viewConfig = knalledgeMapViewService.get().config;
+      this.policyConfig = knalledgeMapPolicyService.get().config;
+      this.globalEmitterServicesArray.register(this.knalledgeNodeTypeChanged);
+      // this.globalEmitterServicesArray.get(this.knalledgeNodeTypeChanged).subscribe('IbisTypesList', function(vkNode,type) {
+      //     console.log("knalledgeNodeTypeChanged: ", vkNode.kNode.name, type);
+      // });
+  }
+
+  selectItem (item) {
+    this.selectedItem = item;
+    this.ibisTypesService.selectActiveType(item);
+
+    if(this.policyConfig.knalledgeMap){
+      this.policyConfig.knalledgeMap.nextNodeType = null;
+    }
+
+    if(this.viewConfig.states.editingNode){
+      this.globalEmitterServicesArray.get(this.knalledgeNodeTypeChanged)
+      .broadcast('IbisTypesList',{node:this.viewConfig.states.editingNode,type:item.type});
+      //, this.selectedItem
+    }
+  }
+
+  hideShowComponent (){
+    this.componentShown = !this.componentShown;
+  }
+}
---
+++ src/frontend/dev_puzzles/ibis/js/services.js
@@ -0,0 +1,23 @@
+/**
+* the namespace for the notify part of the KnAllEdge system
+* @namespace cf.puzzles.ibis
+*/
+
+(function () { // This prevents problems when concatenating scripts that aren't strict.
+'use strict';
+//this function is strict...
+
+var ibisServices = angular.module('ibisServices', []);
+
+/**
+* the namespace for core services for the Gardening system
+* @namespace cf.puzzles.ibis.ibisServices
+*/
+
+/**
+* Service that is a plugin into cf.puzzles.MapVisualization
+* @class ApprovalNodeService
+* @memberof cf.puzzles.ibis.ibisServices
+*/
+
+}()); // end of 'use strict';
